7d285129016cf01f077d29186b978350669bdb17
develper21 <githubnarvin@gmail.com>
2025-10-12 09:06:37 +0530

    Add app new uncomments code

--- growui/src/app/layout.tsx
+++ growui/src/app/layout.tsx
@@ -1,17 +1,15 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { CssBaseline } from "@mui/material";
-import Navbar from "@/components/Navbar"; // Import the new client component
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// metadata export can only be in a Server Component
 export const metadata = {
-  title: "Mutual Fund Explorer",
-  description: "Mutual Fund Explorer with SIP Calculator",
+  title: "Grow",
+  description: "Grow with SIP Calculator",
 };
 
-// RootLayout remains a Server Component
 export default function RootLayout({
   children,
 }: {
@@ -21,11 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <CssBaseline />
-
-        {/* Navigation Panel is now a separate Client Component */}
         <Navbar />
-
-        {/* Page content */}
         <main>{children}</main>
       </body>
     </html>
