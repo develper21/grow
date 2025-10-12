a4135cacf996fb13bc0d4e8af7a4ecdef96de14b
develper21 <githubnarvin@gmail.com>
2025-10-12 09:07:07 +0530

    Add hooks new uncomments code

--- growui/src/hooks/useDebounce.ts
+++ growui/src/hooks/useDebounce.ts
@@ -2,26 +2,18 @@
 
 import { useState, useEffect } from 'react';
 
-/**
- * A custom hook to debounce a value.
- * @param value The value to debounce.
- * @param delay The debounce delay in milliseconds.
- * @returns The debounced value.
- */
 export function useDebounce<T>(value: T, delay: number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
-    // Set a timer to update the debounced value after the specified delay
     const handler = setTimeout(() => {
       setDebouncedValue(value);
     }, delay);
 
-    // Cleanup function to clear the timer if the value changes before the delay has passed
     return () => {
       clearTimeout(handler);
     };
-  }, [value, delay]); // Only re-call effect if value or delay changes
+  }, [value, delay]);
 
   return debouncedValue;
 }
