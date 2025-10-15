c10e8b5ac619cf5e530ebee1189f519bbc61328c
develper21 <githubnarvin@gmail.com>
2025-10-15 12:35:06 +0530

    update a _app.tsx file

--- growui/src/pages/_app.tsx
+++ growui/src/pages/_app.tsx
@@ -1,16 +1,19 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { SessionProvider } from 'next-auth/react';
 import { theme } from '@/theme/theme';
 import Layout from '@/components/Layout';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <SessionProvider session={session}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
